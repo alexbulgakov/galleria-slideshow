9d02ef4966940eb6cf2b53aa1219d284cdedab74
alexbulgakov <contact.bulgakov@gmail.com>
2024-01-14 01:44:53 +0700

    feat(header): added adaptive header and configured global styles in accordance with the design layout

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,10 @@ import styles from './header.module.css';
 function Header() {
   return (
     <div className={styles.header}>
-      <img src={data[0].images.thumbnail} />
+      <img src="src/assets/logo.svg" className={styles.logo} alt="logo" />
+      <a className={styles.startSlideshow} href="#">
+        START SLIDESHOW
+      </a>
     </div>
   );
 }
