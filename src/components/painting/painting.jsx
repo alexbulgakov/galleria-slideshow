18abb30dbbcf88e2ea28ba8ebe9590d41660b837
alexbulgakov <contact.bulgakov@gmail.com>
2024-01-19 06:08:40 +0700

    feat(painting component): created painting component layout

--- src/components/painting/painting.jsx
+++ src/components/painting/painting.jsx
@@ -2,29 +2,38 @@ import styles from './painting.module.css';
 
 function Painting({ painting }) {
   return (
-    <main className={styles.painting}>
+    <div className={styles.painting}>
       <section className={styles.images}>
         <img
           src={painting.images.hero.large}
           className={styles.image}
           alt={painting.name}
         />
-        <div>
-          <h1>{painting.name}</h1>
-          <p>{painting.artist.name}</p>
+        <div className={styles.nameAndArtist}>
+          <h1 className={styles.name}>{painting.name}</h1>
+          <p className={styles.artist}>{painting.artist.name}</p>
         </div>
-        <img src={painting.artist.image} alt={painting.artist.name} />
+        <img
+          className={styles.artistPhoto}
+          src={painting.artist.image}
+          alt={painting.artist.name}
+        />
       </section>
       <section className={styles.about}>
-        <p className={styles.year}>{painting.year}</p>
-        <article>
+        <article className={styles.history}>
+          <p className={styles.year}>{painting.year}</p>
           <p className={styles.description}>{painting.description}</p>
-          <a href={painting.source} rel="noreferrer" target="_blank">
-            GO TO SOURCE
-          </a>
         </article>
+        <a
+          className={styles.link}
+          href={painting.source}
+          rel="noreferrer"
+          target="_blank"
+        >
+          GO TO SOURCE
+        </a>
       </section>
-    </main>
+    </div>
   );
 }
 
